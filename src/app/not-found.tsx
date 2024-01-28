55f882e50be117ce4e61c126dd5c733405412b5b
Gemmuel <gemmueldelacruz@gmail.com>
2024-01-28 20:53:33 +0800

    add: not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="flex h-[80vh] w-full flex-col items-center justify-center ">
+      <h1 className="text-9xl font-bold">404</h1>
+      <h2 className="mt-4 text-3xl">Page not found.</h2>
+      <p className="mt-2 text-lg">
+        Sorry, we can't find that page. You'll find lots to explore on the home
+        page.
+      </p>
+      <Link href="/">
+        <Button className="mt-8 rounded px-6 py-4">Back to Homepage</Button>
+      </Link>
+    </div>
+  );
+}
