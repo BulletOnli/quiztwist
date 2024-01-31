370f11c71089db9c01d2a8ddc059cce2196472b8
Gemmuel <gemmueldelacruz@gmail.com>
2024-01-31 09:13:53 +0800

    update metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,16 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 const LandingPage = () => {
-    return (
-        <div className="w-full h-screen flex justify-center items-center text-3xl">
-            <div className="flex flex-col items-center">
-                <h1 className="mb-4">Landing Page</h1>
-                <Link href="/dashboard">
-                    <Button size="lg">Go to Dashboard</Button>
-                </Link>
-            </div>
-        </div>
-    );
+  return (
+    <div className="w-full h-screen flex justify-center items-center text-3xl">
+      <div className="flex flex-col items-center">
+        <h1 className="mb-4">Landing Page</h1>
+        <Link href="/dashboard">
+          <Button size="lg">Go to Dashboard</Button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
