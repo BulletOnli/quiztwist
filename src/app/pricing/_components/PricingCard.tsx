e0901d9f2a2e3dcaeccd0bf8b6763f7dca275549
Gemmuel <gemmueldelacruz@gmail.com>
2024-02-28 10:12:41 +0800

    shesh

--- src/app/pricing/_components/PricingCard.tsx
+++ src/app/pricing/_components/PricingCard.tsx
@@ -14,7 +14,7 @@ type PricingCardType = {
 const PricingCard = ({ cardPrice, cardTitle, perks }: PricingCardType) => {
   const { data: session } = useSession();
 
-  const handleSubmit = async () => {
+  const handleClick = async () => {
     await stripeCheckout({
       plan: cardTitle,
       price: cardPrice,
@@ -24,60 +24,58 @@ const PricingCard = ({ cardPrice, cardTitle, perks }: PricingCardType) => {
 
   return (
     <div className="group relative md:col-span-1 lg:w-[32%]">
-      <form action={handleSubmit}>
-        <div
-          aria-hidden="true"
-          className="absolute top-0 h-full w-full rounded-3xl border border-gray/20 dark:border-gray-700 bg-white dark:bg-gray-800 shadow-2xl shadow-gray-600/10 dark:shadow-none transition duration-500 group-hover:scale-105 lg:group-hover:scale-110"
-        ></div>
-        <div className="relative space-y-8 p-8">
-          <h3 className="text-center text-3xl font-semibold text-darkGray/90 dark:text-white">
-            {cardTitle}
-          </h3>
-          <div className="overflow-hidden">
-            <div className="relative flex justify-around">
-              <div className="flex">
-                <span className="-ml-6 mt-2 text-3xl font-bold text-primary">
-                  $
-                </span>
-                <span className="leading-0 text-8xl font-bold text-darkGray dark:text-white">
-                  {cardPrice}
-                </span>
-              </div>
-            </div>
-            {cardTitle.toLowerCase() === "premium" && (
-              <span className="block text-2xl font-medium text-center text-gray-400 line-through mt-3">
-                $4.99
+      <div
+        aria-hidden="true"
+        className="absolute top-0 h-full w-full rounded-3xl border border-gray/20 dark:border-gray-700 bg-white dark:bg-gray-800 shadow-2xl shadow-gray-600/10 dark:shadow-none transition duration-500 group-hover:scale-105 lg:group-hover:scale-110"
+      ></div>
+      <div className="relative space-y-8 p-8">
+        <h3 className="text-center text-3xl font-semibold text-darkGray/90 dark:text-white">
+          {cardTitle}
+        </h3>
+        <div className="overflow-hidden">
+          <div className="relative flex justify-around">
+            <div className="flex">
+              <span className="-ml-6 mt-2 text-3xl font-bold text-primary">
+                $
               </span>
-            )}
-            <span className="block text-center text-xs uppercase text-primary">
-              BILLED MONTHLY
-            </span>
+              <span className="leading-0 text-8xl font-bold text-darkGray dark:text-white">
+                {cardPrice}
+              </span>
+            </div>
           </div>
-
-          <ul
-            role="list"
-            className="m-auto w-max space-y-4 pb-6 text-gray-600 dark:text-gray-300"
-          >
-            {perks?.map((perk) => (
-              <li
-                key={perk + Math.random()}
-                className="flex items-center space-x-2"
-              >
-                <Check className="text-darkGray w-5 h-5 " />
-                <span>{perk}</span>
-              </li>
-            ))}
-          </ul>
-          <Button
-            type="submit"
-            className="w-full  rounded-full font-semibold text-white dark:text-darkGray"
-          >
-            {session?.user.subscription === "pro"
-              ? "Current plan"
-              : "Start a plan"}
-          </Button>
+          {cardTitle.toLowerCase() === "premium" && (
+            <span className="block text-2xl font-medium text-center text-gray-400 line-through mt-3">
+              $4.99
+            </span>
+          )}
+          <span className="block text-center text-xs uppercase text-primary">
+            BILLED MONTHLY
+          </span>
         </div>
-      </form>
+
+        <ul
+          role="list"
+          className="m-auto w-max space-y-4 pb-6 text-gray-600 dark:text-gray-300"
+        >
+          {perks?.map((perk) => (
+            <li
+              key={perk + Math.random()}
+              className="flex items-center space-x-2"
+            >
+              <Check className="text-darkGray w-5 h-5 " />
+              <span>{perk}</span>
+            </li>
+          ))}
+        </ul>
+        <Button
+          onClick={handleClick}
+          className="w-full  rounded-full font-semibold text-white dark:text-darkGray"
+        >
+          {session?.user.subscription === "pro"
+            ? "Current plan"
+            : "Start a plan"}
+        </Button>
+      </div>
     </div>
   );
 };
