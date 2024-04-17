eb4c76f6d12fadb7f1c3de6170bee8f13754330b
Gemmuel <gemmueldelacruz@gmail.com>
2024-04-17 22:04:42 +0800

    fix: students can access add and export

--- src/app/r/[roomId]/(room)/people/page.tsx
+++ src/app/r/[roomId]/(room)/people/page.tsx
@@ -5,27 +5,34 @@ import { Button } from "@/components/ui/button";
 import { getClassroomData } from "@/lib/actions/classroom.actions";
 import Link from "next/link";
 import { Download } from "lucide-react";
+import { getServerSession } from "next-auth";
+import authOptions from "@/utils/authOptions";
 
 type PeoplePageProps = {
   params: { roomId: string };
 };
 
 const PeoplePage = async ({ params }: PeoplePageProps) => {
+  const session = await getServerSession(authOptions);
+  const isStudent = session?.user.role === "Student";
+
   const classroom = await getClassroomData(params.roomId);
 
   return (
     <main className="w-full max-w-7xl mx-auto flex flex-col p-10 ">
-      <div className="w-full flex items-center justify-between gap-1 mb-6">
-        <AddStudentsModal roomId={params.roomId} />
-        <Button asChild variant="outline" className="w-full">
-          <Link
-            href={`/api/export/students/${classroom?._id}`}
-            target="_parent"
-          >
-            <Download className="w-5 h-5 mr-2" /> Export Students
-          </Link>
-        </Button>
-      </div>
+      {!isStudent && (
+        <div className="w-full flex items-center justify-between gap-1 mb-6">
+          <AddStudentsModal roomId={params.roomId} />
+          <Button asChild variant="outline" className="w-full">
+            <Link
+              href={`/api/export/students/${classroom?._id}`}
+              target="_parent"
+            >
+              <Download className="w-5 h-5 mr-2" /> Export Students
+            </Link>
+          </Button>
+        </div>
+      )}
       <div className="w-full flex flex-col gap-4">
         <div className="flex items-center justify-between">
           <h1 className="text-xl font-medium">Teacher</h1>
